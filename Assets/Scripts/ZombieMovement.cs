fe6dda480d776bd21c1679eb7cceaf2d8baab338
aykut-ozer <aykut-ozer@hotmail.com>
2023-05-12 04:30:04 +0300

    Zombie tilt fixed
    Zombilerin "x" ekseninde dönmesi düzeltildi.

--- Assets/Scripts/ZombieMovement.cs
+++ Assets/Scripts/ZombieMovement.cs
@@ -5,23 +5,29 @@ using UnityEngine;
 public class ZombieMovement : MonoBehaviour
 {
     GameObject wagon;
-    Transform zombieTransform;
+    Vector3 zombieSize;
+    Vector3 target;
+    //Transform zombieTransform;
 
     void Start()
     {
         wagon = GameObject.Find("Wagon");
+        Vector3 zombieSize= GetComponent<Collider>().bounds.size;
         Debug.Log(wagon.transform.position);
     }
 
     void Update()
     {
         ZombieRotation();
-
+        
     }
 
 
     void ZombieRotation() 
     {
-        transform.LookAt(wagon.transform);
+        target= new Vector3 (wagon.transform.position.x,zombieSize.y, wagon.transform.position.z);
+        transform.LookAt(target);
+        
     }
+
 }
