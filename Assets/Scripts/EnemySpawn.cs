4814f959fef2d54c181e8dbd850f661f63971112
resul <r.evlendi@gmail.com>
2023-05-20 01:14:56 +0300

    Scripts Edit 3
    spawn posizyonları, hedefler, spawn olan zombiler dizilere alındı ve bölgelerine göre hedef seçimleri yapıldı

--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -14,11 +14,12 @@ public class EnemySpawn : MonoBehaviour
     public int spawnAdeti;
     internal GameObject target;
     float x, y, z;
-
+    GameObjects array;
 
 
     private void Awake()
     {
+        array = FindObjectOfType<GameObjects>();
         Spawner();
 
     }
@@ -26,20 +27,59 @@ public class EnemySpawn : MonoBehaviour
 
     void Spawner() //belirli zaman araliginda obje spawn etme
     {
-    
+
         StartCoroutine(kk());
 
     }
 
+    internal void EnemyInstant()
+    {
+        //Olusacak objeye rastgele bir bolge belirlenmesi
 
-    void SpawnPos()
+        sayac++;
+        location = Random.Range(1, 4);
+        switch (location)
+        {
+            case 1:
+                LeftSpawnPos();
+                array.leftSpawn.Add(Instantiate(enemy, left, Quaternion.identity)); //sol bolge spawn
+                break;
+            case 2:
+                RightSpawnPos();
+                array.rightSpawn.Add(Instantiate(enemy, right, Quaternion.identity)); //sag bolge spawn
+                break;
+            case 3:
+                BackSpawnPos();
+                array.backSpawn.Add(Instantiate(enemy, back, Quaternion.identity)); // arka bolge spawn
+                break;
+        }
+
+
+    }
+
+    IEnumerator kk()
+    {
+
+
+
+        while (sayac < spawnAdeti)
+        {
+
+            EnemyInstant();
+
+            yield return new WaitForSeconds(2f);
+        }
+
+    }
+
+    void RightSpawnPos()
     {
 
         //Sag bolge spawn posizyonlari
 
-        GameObject R1 = GameObject.Find("right1"),
-                   R2 = GameObject.Find("right2"),
-                   R3 = GameObject.Find("right3");
+        GameObject R1 = array.spawnLocat[0],
+                   R2 = array.spawnLocat[1],
+                   R3 = array.spawnLocat[2];
 
         int r = Random.Range(1, 4);
 
@@ -48,25 +88,31 @@ public class EnemySpawn : MonoBehaviour
             case 1:
                 R1.transform.position = new Vector3(Random.Range(R1.transform.position.x, R1.transform.position.x - 2), enemy.transform.position.y, Random.Range(R1.transform.position.z - 3, R1.transform.position.z + 3));
                 right = R1.transform.position;
+                target = array.targetLocat[0];
                 break;
 
             case 2:
                 R2.transform.position = new Vector3(Random.Range(R2.transform.position.x, R2.transform.position.x - 2), enemy.transform.position.y, Random.Range(R2.transform.position.z - 3, R2.transform.position.z + 3));
                 right = R2.transform.position;
+                target = array.targetLocat[1];
                 break;
 
             case 3:
                 R3.transform.position = new Vector3(Random.Range(R3.transform.position.x, R3.transform.position.x - 2), enemy.transform.position.y, Random.Range(R3.transform.position.z - 3, R3.transform.position.z + 3));
                 right = R3.transform.position;
+                target = array.targetLocat[2];
                 break;
         }
 
+    }
+    void LeftSpawnPos()
+    {
 
         //Sol bolge spawn posizyonlari
 
-        GameObject L1 = GameObject.Find("left1"),
-                   L2 = GameObject.Find("left2"),
-                   L3 = GameObject.Find("left3");
+        GameObject L1 = array.spawnLocat[3],
+                   L2 = array.spawnLocat[4],
+                   L3 = array.spawnLocat[5];
 
         int l = Random.Range(1, 4);
 
@@ -77,25 +123,30 @@ public class EnemySpawn : MonoBehaviour
             case 1:
                 L1.transform.position = new Vector3(Random.Range(L1.transform.position.x, L1.transform.position.x + 2), enemy.transform.position.y, Random.Range(L1.transform.position.z - 3, L1.transform.position.z + 3));
                 left = L1.transform.position;
+                target = array.targetLocat[3];
                 break;
 
             case 2:
                 L2.transform.position = new Vector3(Random.Range(L2.transform.position.x, L2.transform.position.x + 2), enemy.transform.position.y, Random.Range(L2.transform.position.z - 3, L2.transform.position.z + 3));
                 left = L2.transform.position;
+                target = array.targetLocat[4];
                 break;
 
             case 3:
                 L3.transform.position = new Vector3(Random.Range(L3.transform.position.x, L3.transform.position.x + 2), enemy.transform.position.y, Random.Range(L3.transform.position.z - 3, L3.transform.position.z + 3));
                 left = L3.transform.position;
+                target = array.targetLocat[5];
                 break;
         }
-
+    }
+    void BackSpawnPos()
+    {
 
         // Arka bolge spawn posizyonlarý
 
-        GameObject B1 = GameObject.Find("back1"),
-                   B2 = GameObject.Find("back2"),
-                   B3 = GameObject.Find("back3");
+        GameObject B1 = array.spawnLocat[6],
+                   B2 = array.spawnLocat[7],
+                   B3 = array.spawnLocat[8];
 
         int b = Random.Range(1, 4);
 
@@ -104,127 +155,20 @@ public class EnemySpawn : MonoBehaviour
             case 1:
                 B1.transform.position = new Vector3(Random.Range(B1.transform.position.x - 3, B1.transform.position.x + 3), enemy.transform.position.y, Random.Range(B1.transform.position.z, B1.transform.position.z - 2));
                 back = B1.transform.position;
+                target = array.targetLocat[6];
                 break;
 
             case 2:
                 B2.transform.position = new Vector3(Random.Range(B2.transform.position.x - 3, B2.transform.position.x + 3), enemy.transform.position.y, Random.Range(B2.transform.position.z, B2.transform.position.z - 2));
                 back = B2.transform.position;
+                target = array.targetLocat[Random.Range(6, 8)];
                 break;
 
             case 3:
                 B3.transform.position = new Vector3(Random.Range(B3.transform.position.x - 3, B3.transform.position.x + 3), enemy.transform.position.y, Random.Range(B3.transform.position.z, B3.transform.position.z - 2));
                 back = B3.transform.position;
+                target = array.targetLocat[7];
                 break;
-        }
-
-
-    }
-
-
-
-    internal void EnemyInstant()
-    {
-        //Olusacak objeye rastgele bir bolge belirlenmesi
-        sayac++;
-        SpawnPos();
-        location = Random.Range(1, 4);
-        switch (location)
-        {
-            case 1:
-                Instantiate(enemy, left, Quaternion.identity); //sol bolge spawn
-                TargetLeft();
-                break;
-            case 2:
-                Instantiate(enemy, right, Quaternion.identity); //sag bolge spawn
-                TargetRight();
-                break;
-            case 3:
-                Instantiate(enemy, back, Quaternion.identity); // arka bolge spawn
-                TargetBack();
-                break;
-        }
-
-       
-    }
-
-
-
-    void TargetLeft()
-    {
-
-
-        x = transform.position.z - GameObject.Find("lt1").transform.position.z;
-        y = transform.position.z - GameObject.Find("lt2").transform.position.z;
-        z = transform.position.z - GameObject.Find("lt3").transform.position.z;
-
-
-
-        if (x < y & x < z)
-        {
-            target = GameObject.Find("lt1");
-            Debug.Log("first");
-        }
-
-        else if (y < x & y < z)
-        {
-            target = GameObject.Find("lt2");
-            Debug.Log("seconds");
-        }
-        else if (z < x & z < y)
-        {
-            target = GameObject.Find("lt3");
-            Debug.Log("third");
-        }
-
-
-    }
-
-    void TargetRight()
-    {
-
-
-        x = transform.position.z - GameObject.Find("rt1").transform.position.z;
-        y = transform.position.z - GameObject.Find("rt2").transform.position.z;
-        z = transform.position.z - GameObject.Find("rt3").transform.position.z;
-
-
-
-        if (x < y & x < z)
-            target = GameObject.Find("rt1");
-        else if (y < x & y < z)
-            target = GameObject.Find("rt2");
-        else if (z < x & z < y)
-            target = GameObject.Find("rt3");
-
-    }
-
-    void TargetBack()
-    {
-
-
-        x = transform.position.x - GameObject.Find("blt").transform.position.x;
-        y = transform.position.x - GameObject.Find("brt").transform.position.x;
-
-
-
-        if (x < y)
-            target = GameObject.Find("blt");
-        else if (y < x)
-            target = GameObject.Find("brt");
-
-    }
-
-    IEnumerator kk()
-    {
-        
-
-
-        while (sayac < spawnAdeti)
-        {
-
-            EnemyInstant();
-
-            yield return new WaitForSeconds(2f);
         }
 
     }
---
+++ Assets/Scripts/GameObjects.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjects : MonoBehaviour
+{
+   public GameObject[] spawnLocat = new GameObject[9];
+   public GameObject[] targetLocat = new GameObject[8];
+   public List<GameObject> rightSpawn ;
+   public List<GameObject> leftSpawn ;
+   public List<GameObject> backSpawn ;
+
+
+    void Start()
+    {
+        
+    }
+
+
+
+
+
+    void Update()
+    {
+        
+    }
+}
